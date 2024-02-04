17b21807d1e5c743ace1b896d9b9b2fdb72b44ed
obi199 <pebellinghausen@posteo.de>
2024-02-04 13:56:41 +0100

    update main.py

--- main.cpp
+++ main.cpp
@@ -18,13 +18,13 @@ int main(int argc, char* argv[])
 
     if (argc != 3)
     {
-        cerr << "Run in folder with files! \nError: no parameters declared. Here is the command list: \n16bitwav2snd (filename) or (-all)\nanywav2snd (filename) or (-all)\nwav16bit\nConvertSnd2Wav (filename) or (-all)" << endl;
+        cerr << "Run in folder with files! \nError: Run in folder with files params(filename) or (-all) !\nwav2snd      converts 16bit wav to snd \nanywav2snd    converts any wav to snd\nwav16bit    converts wav to 16bit\nsnd2wav       converts snd to wav\n" << endl;
         return 1;
     }
     string param = std::string(argv[1]);
     string nfile = std::string(argv[2]);
 
-    if (param == "16bitwav2snd")
+    if (param == "wav2snd")
     {   
         numFiles = convert16Bitwav2snd(nfile);
     }
@@ -42,7 +42,7 @@ int main(int argc, char* argv[])
     }
     else
     {
-        std::cout << "Error: Run in folder with files! Commands: \n16bitwav2snd (filename) or (-all)\nanywav2snd (filename) or (-all)\nConvertSnd2Wav (filename) or (-all)";
+        std::cout << "Error: Run in folder with files params (filename) or (-all) !\n16bitwav2snd\nanywav2snd\nwav16bit\nsnd2wav\n";
     }
     cout << numFiles << " Files converted" << endl;
 
